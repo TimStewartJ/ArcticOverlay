b28b93a6ff14e85b85b2e1c24dd634c1d0df224f
Tim <timstewartj@gmail.com>
2021-11-08 12:57:19 -0800

    add table sorting

--- src/renderer.js
+++ src/renderer.js
@@ -1,5 +1,35 @@
 const fields = ['PLAYER','TAG', 'WS', 'FKDR', 'WLR'];
 
+const sortTable = (tableName) => {
+    const table = document.getElementById(tableName);
+    let rows, i, x, y, shouldSwitch;
+    let switching = true;
+    while(switching) {
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < (rows.length - 1); i++) {
+            // Start by saying there should be no switching:
+            shouldSwitch = false;
+            /* Get the two elements you want to compare,
+            one from current row and one from the next: */
+            x = rows[i].getElementsByTagName("TD")[3];
+            y = rows[i + 1].getElementsByTagName("TD")[3];
+            // Check if the two rows should switch place:
+            if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                // If so, mark as a switch and break the loop:
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark that a switch has been done: */
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        }
+    }
+}
+
 window.players.display('showPlayers', (data) => {
     let table = document.getElementById("main-table");
     table.innerHTML = ''; // reset table
@@ -38,6 +68,7 @@ window.players.update('updatePlayer', (data) => {
         }
         playerRow.innerHTML += `<td>${fieldContent}</td>`
     })
+    sortTable("main-table");
 })
 
 document.getElementById('start-reading').addEventListener('click', async () => {
